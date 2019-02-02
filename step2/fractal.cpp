7626aee4d8002e03004f487e6aadaf4a96bb33b2
wrestlerdude <raishallan2@gmail.com>
2019-02-02 15:21:33 +0000

    Update fractal.cpp

--- step2/fractal.cpp
+++ step2/fractal.cpp
@@ -1,7 +1,27 @@
 #include <CImg/CImg.h>
+#include <cmath>
+#include <vector>
 
-void draw_tree(CImg<unsigned char> &image, int i, int t, int r, double theta, double dtheta)
+using namespace std;
+using namespace cimg_library;
+
+vector<tuple<int, int, int, int>> fractal_tree(CImg<unsigned char> &image, int &i, int &ox, int &oy, int &t, int &r, double &theta, double &dtheta)
 {
 	if (i == 0)
-		return;
+		return nullptr;
+	int x0 = ox;
+	int y0 = oy;
+	int x = x0 + (t * cos(theta));
+	int y = y0 + (t * sin(theta));
+
+	vector<tuple<int, int, int, int>> lines;
+	vector<tuple<int, int, int, int>> lineA;
+	vector<tuple<int, int, int, int>> lineB;
+	lines.push_back(make_tuple(x0, y0, x, y));
+	lineA = fractal_tree(i-1, x, y, t * r, r, theta + dtheta, dtheta);
+	lines.insert(lines.end(), lineA.begin(), lineA.end());
+	lineB = fractal_tree(i-1, x, y, t * r, theta - dtheta, dtheta);
+	lines.insert(lines.end(), lineB.begin(), lineB.end());
+
+	return lines;
 }
--- step2/main.cpp
+++ step2/main.cpp
@@ -27,12 +27,16 @@ int main()
 	cout << seed << endl;
 	CImg<unsigned char> image(640, 480, 1, 3, 0);
 	unsigned char colors[3];
+
 	colors[0] = 0xff;
 	colors[1] = 0;
 	colors[2] = 0;
 	CImgDisplay display(image, "Window");
-	image.draw_point(0, 0, colors);
-	image.display(display);
+//	image.draw_line(0, 0, 100, 100, colors);
+	while (!display.is_closed())
+	{
+		image.display(display);
+	}
 
     return 0;
 }
