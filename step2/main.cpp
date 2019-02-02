0afe786c2db331ae1326a85a0565d2e8f3894ff2
wrestlerdude <raishallan2@gmail.com>
2019-02-02 13:12:04 +0000

    Updated paths for gitignore

---
+++ step2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string encodedString;
+	cout << "Enter encoded goldberg string:\n";
+	getline(cin, encodedString);	
+
+    return 0;
+}
