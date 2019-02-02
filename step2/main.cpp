9754de9681f04052ef3d773521d4ff5c0a97e0a6
wrestlerdude <raishallan2@gmail.com>
2019-02-02 14:15:59 +0000

    updated main.cpp

--- step2/main.cpp
+++ step2/main.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <cstdio>
+#include <cstdint>
+#include "cpp-base64/base64.h"
+#include "fractal.h"
 
 using namespace std;
 
 int main()
 {
-	string encodedString;
-	cout << "Enter encoded goldberg string:\n";
-	getline(cin, encodedString);	
+	string encoded;
+	cout << "Enter encoded rube goldberg string:\n";
+	getline(cin, encoded);
+	string decoded = base64_decode(encoded);
+	reverse(decoded.begin(), decoded.end());
+	cout << decoded << "\n";
+
+	size_t seed = 0;
+
+	for(auto &c : decoded)
+		seed += c;
+
+	cout << seed << endl;
 
     return 0;
 }
