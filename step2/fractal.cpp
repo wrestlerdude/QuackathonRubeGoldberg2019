118f91b889815bd6f1197e719824180feeb5e250
wrestlerdude <raishallan2@gmail.com>
2019-02-02 19:15:11 +0000

    Fractal.cpp outputs image

--- step2/fractal.cpp
+++ step2/fractal.cpp
@@ -19,16 +19,16 @@ void pattern(CImg<unsigned char> &image, double seed)
 			int c = (int)trunc(x*x + y*y);
 			if (c % 2 == 0)
 				image(i, j, 0) = 0xff;
-			if (c % 3 == 0)
-				image(i, j , 1) = 0xff;
+			else if (c % 3 == 0)
+				image(i, j, 1) = 0xff;
+			else if (c % 10 == 0)
+				image(i, j, 2) = 0xff;
 		}
 }
 
 void render_fractal(int seed, int width, int height)
 {
 	CImg<unsigned char> image(width, height, 1, 3, 0);
-	CImgDisplay display(image, "Fractal");
 	pattern(image, (double)seed);
-	while (!display.is_closed())
-		image.display(display);
+	image.save("output.jpg");
 }
